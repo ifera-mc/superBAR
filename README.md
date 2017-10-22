497aaec47ad8b99392d60f2fd66ccc6c75130e0f
Jack M. Taylor <32965703+JackMD@users.noreply.github.com>
2017-10-22 22:27:12 +0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # superBAR
-Just good and customizable HUD for PocketMine-based servers. Originally created by @FaigerSYS. Updated for PocketMine-MP 1.2 by @JackMD.
+***
+
+###  Originally created by @FaigerSYS. Updated for PocketMine-MP 1.2 by @JackMD.
+
+To download, look [here](https://github.com/JackMD/superBAR/releases).
+
+All setup instructions you can see in the [config](https://github.com/JackMD/superBAR/blob/master/resources/config.yml).
+
+## License
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.  
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.  
+You should have received a copy of the GNU General Public License along with this program. [If not, click here.](http://www.gnu.org/licenses/)
---
+++ src/FaigerSYS/superBAR/controller/EventController.php
@@ -0,0 +1,38 @@
+<?php
+namespace FaigerSYS\superBAR\controller;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerRespawnEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use FaigerSYS\superBAR\BaseModule;
+
+class EventController extends BaseModule implements Listener {
+
+	/**
+	 * @param PlayerJoinEvent $e
+	 * @priority MONITOR
+	 */
+	public function onJoin(PlayerJoinEvent $e) {
+		$display = ($this->getPlugin()->isDefaultEnabled() && $this->getPlugin()->hasPermission($player = $e->getPlayer(), 'use'));
+		$this->getPlugin()->getHUD()->setDisplay($player->getName(), $display);
+	}
+
+	/**
+	 * @param PlayerRespawnEvent $e
+	 * @priority MONITOR
+	 */
+	public function onRespawn(PlayerRespawnEvent $e) {
+		$display = ($this->getPlugin()->isDefaultEnabled() && $this->getPlugin()->hasPermission($player = $e->getPlayer(), 'use'));
+		$this->getPlugin()->getHUD()->setDisplay($player->getName(), $display);
+	}
+	
+	/**
+	 * @param PlayerQuitEvent $e
+	 * @priority MONITOR
+	 */
+	public function onLogout(PlayerQuitEvent $e) {
+		$this->getPlugin()->getHUD()->setDisplay($e->getPlayer()->getName(), false);
+	}
+	
+}
