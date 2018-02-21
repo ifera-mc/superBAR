faa77e33f77f8b44f78167301937b3df968cade8
Jack M. Taylor <32965703+JackMD@users.noreply.github.com>
2018-02-21 18:41:41 +0500

    Reformat and PHPDoc updates

--- src/FaigerSYS/superBAR/BaseModule.php
+++ src/FaigerSYS/superBAR/BaseModule.php
@@ -1,16 +1,26 @@
 <?php
+
 namespace FaigerSYS\superBAR;
 
-abstract class BaseModule {
-	
-	private static $plugin = null;
-	
-	protected function getPlugin() {
-		return BaseModule::$plugin;
-	}
-	
-	public static function setPlugin(superBAR $plugin) {
-		BaseModule::$plugin = $plugin;
-	}
-	
+abstract class BaseModule
+{
+
+    private static $plugin = null;
+
+    /**
+     * @param superBAR $plugin
+     */
+    public static function setPlugin(superBAR $plugin)
+    {
+        BaseModule::$plugin = $plugin;
+    }
+
+    /**
+     * @return null
+     */
+    protected function getPlugin()
+    {
+        return BaseModule::$plugin;
+    }
+
 }
