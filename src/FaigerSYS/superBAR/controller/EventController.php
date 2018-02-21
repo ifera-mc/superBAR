4dc08103ca2357e9ce40d583bf59f6942c0d9786
Jack M. Taylor <32965703+JackMD@users.noreply.github.com>
2018-02-21 18:55:23 +0500

    Update EventController.php

--- src/FaigerSYS/superBAR/controller/EventController.php
+++ src/FaigerSYS/superBAR/controller/EventController.php
@@ -1,38 +1,43 @@
 <?php
+
 namespace FaigerSYS\superBAR\controller;
 
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerRespawnEvent;
-use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\event\player\PlayerJoinEvent;
 use FaigerSYS\superBAR\BaseModule;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\player\PlayerRespawnEvent;
 
-class EventController extends BaseModule implements Listener {
+class EventController extends BaseModule implements Listener
+{
 
-	/**
-	 * @param PlayerJoinEvent $e
-	 * @priority MONITOR
-	 */
-	public function onJoin(PlayerJoinEvent $e) {
-		$display = ($this->getPlugin()->isDefaultEnabled() && $this->getPlugin()->hasPermission($player = $e->getPlayer(), 'use'));
-		$this->getPlugin()->getHUD()->setDisplay($player->getName(), $display);
-	}
+    /**
+     * @param PlayerJoinEvent $e
+     * @priority MONITOR
+     */
+    public function onJoin(PlayerJoinEvent $e)
+    {
+        $display = ($this->getPlugin()->isDefaultEnabled() && $this->getPlugin()->hasPermission($player = $e->getPlayer(), 'use'));
+        $this->getPlugin()->getHUD()->setDisplay($player->getName(), $display);
+    }
 
-	/**
-	 * @param PlayerRespawnEvent $e
-	 * @priority MONITOR
-	 */
-	public function onRespawn(PlayerRespawnEvent $e) {
-		$display = ($this->getPlugin()->isDefaultEnabled() && $this->getPlugin()->hasPermission($player = $e->getPlayer(), 'use'));
-		$this->getPlugin()->getHUD()->setDisplay($player->getName(), $display);
-	}
-	
-	/**
-	 * @param PlayerQuitEvent $e
-	 * @priority MONITOR
-	 */
-	public function onLogout(PlayerQuitEvent $e) {
-		$this->getPlugin()->getHUD()->setDisplay($e->getPlayer()->getName(), false);
-	}
-	
+    /**
+     * @param PlayerRespawnEvent $e
+     * @priority MONITOR
+     */
+    public function onRespawn(PlayerRespawnEvent $e)
+    {
+        $display = ($this->getPlugin()->isDefaultEnabled() && $this->getPlugin()->hasPermission($player = $e->getPlayer(), 'use'));
+        $this->getPlugin()->getHUD()->setDisplay($player->getName(), $display);
+    }
+
+    /**
+     * @param PlayerQuitEvent $e
+     * @priority MONITOR
+     */
+    public function onLogout(PlayerQuitEvent $e)
+    {
+        $this->getPlugin()->getHUD()->setDisplay($e->getPlayer()->getName(), false);
+    }
+
 }
