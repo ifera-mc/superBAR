72d99507018de42bb1649594ef7cb4e69012f86c
Jack M. Taylor <32965703+JackMD@users.noreply.github.com>
2018-02-21 18:54:49 +0500

    Update DataController.php

--- src/FaigerSYS/superBAR/controller/DataController.php
+++ src/FaigerSYS/superBAR/controller/DataController.php
@@ -1,65 +1,107 @@
 <?php
+
 namespace FaigerSYS\superBAR\controller;
 
 use FaigerSYS\superBAR\core\HUD;
-use FaigerSYS\superBAR\provider\ConfigProvider;
 use FaigerSYS\superBAR\provider\AddonProvider;
+use FaigerSYS\superBAR\provider\ConfigProvider;
 
-class DataController {
-	
-	/** @var \FaigerSYS\superBAR\core\HUD */
-	private $HUD = null;
-	
-	/** @var \FaigerSYS\superBAR\provider\ConfigProvider */
-	private $configProvider = null;
-	
-	/** @var \FaigerSYS\superBAR\provider\AddonProvider */
-	private $addonProvider = null;
-	
-	/** @var \FaigerSYS\superBAR\controller\EventController */
-	private $eventController = null;
-	
-	/** @var bool */
-	private $defaultEnabled = true;
-	
-	public function setHUD(HUD $HUD) {
-		$this->HUD = $HUD;
-	}
-	
-	public function getHUD() {
-		return $this->HUD;
-	}
-	
-	public function setConfigProvider(ConfigProvider $provider) {
-		$this->configProvider = $provider;
-	}
-	
-	public function getConfigProvider() {
-		return $this->configProvider;
-	}
-	
-	public function setAddonProvider(AddonProvider $provider) {
-		$this->addonProvider = $provider;
-	}
-	
-	public function getAddonProvider() {
-		return $this->addonProvider;
-	}
-	
-	public function setEventController(EventController $controller) {
-		$this->eventController = $controller;
-	}
-	
-	public function getEventController() {
-		return $this->eventController;
-	}
-	
-	public function setDefaultEnabled(bool $state) {
-		$this->defaultEnabled = $state;
-	}
-	
-	public function isDefaultEnabled() {
-		return $this->defaultEnabled;
-	}
-	
+class DataController
+{
+
+    /** @var \FaigerSYS\superBAR\core\HUD */
+    private $HUD = null;
+
+    /** @var \FaigerSYS\superBAR\provider\ConfigProvider */
+    private $configProvider = null;
+
+    /** @var \FaigerSYS\superBAR\provider\AddonProvider */
+    private $addonProvider = null;
+
+    /** @var \FaigerSYS\superBAR\controller\EventController */
+    private $eventController = null;
+
+    /** @var bool */
+    private $defaultEnabled = true;
+
+    /**
+     * @return HUD
+     */
+    public function getHUD()
+    {
+        return $this->HUD;
+    }
+
+    /**
+     * @param HUD $HUD
+     */
+    public function setHUD(HUD $HUD)
+    {
+        $this->HUD = $HUD;
+    }
+
+    /**
+     * @return ConfigProvider
+     */
+    public function getConfigProvider()
+    {
+        return $this->configProvider;
+    }
+
+    /**
+     * @param ConfigProvider $provider
+     */
+    public function setConfigProvider(ConfigProvider $provider)
+    {
+        $this->configProvider = $provider;
+    }
+
+    /**
+     * @return AddonProvider
+     */
+    public function getAddonProvider()
+    {
+        return $this->addonProvider;
+    }
+
+    /**
+     * @param AddonProvider $provider
+     */
+    public function setAddonProvider(AddonProvider $provider)
+    {
+        $this->addonProvider = $provider;
+    }
+
+    /**
+     * @return EventController
+     */
+    public function getEventController()
+    {
+        return $this->eventController;
+    }
+
+    /**
+     * @param EventController $controller
+     */
+    public function setEventController(EventController $controller)
+    {
+        $this->eventController = $controller;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isDefaultEnabled()
+    {
+        return $this->defaultEnabled;
+    }
+
+    /**
+     * @param bool $state
+     */
+    public function setDefaultEnabled(bool $state)
+    {
+        $this->defaultEnabled = $state;
+    }
+
 }
